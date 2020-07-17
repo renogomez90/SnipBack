d3d5c326ef1a458d67b5850b23b17560de90bfa8
Git <tomson@q-devengine.com>
2020-07-17 18:35:24 +0530

    Register screen

---
+++ app/src/main/java/com/example/snipback/Activty_Register.java
@@ -0,0 +1,22 @@
+package com.example.snipback;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class Activty_Register extends AppCompatActivity {
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_register);
+
+    }
+
+}
