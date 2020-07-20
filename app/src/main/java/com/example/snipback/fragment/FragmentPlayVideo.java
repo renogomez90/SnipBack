6a0f8114bc43b4c8e4ef046facc1fcd2ffd90ac8
renogomez90 <reno@hipointconnect.com>
2020-07-20 10:29:29 +0530

    videoEditing

--- app/src/main/java/com/example/snipback/fragment/FragmentPlayVideo.java
+++ app/src/main/java/com/example/snipback/fragment/FragmentPlayVideo.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.snipback.AppMainActivity;
 import com.example.snipback.R;
 
 public class FragmentPlayVideo extends Fragment {
     private View rootView;
+    LinearLayout bottom_menu;
 
     public  static  FragmentPlayVideo newInstance() {
         FragmentPlayVideo fragment = new FragmentPlayVideo();
@@ -23,6 +27,14 @@ public class FragmentPlayVideo extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.layout_play_video, container, false);
+
+        bottom_menu=rootView.findViewById(R.id.bottom_menu);
+        bottom_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((AppMainActivity) getActivity()).loadFragment(CreateTag.newInstance());
+            }
+        });
 
         return rootView;
     }
